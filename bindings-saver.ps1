b0f5a0202c47af4616ef3b4b76d44543f7a0fbce
Tony Stork <tm24fan8@gmail.com>
2022-04-30 18:30:48 -0400

    Added warning not to edit other than variables

--- bindings-saver.ps1
+++ bindings-saver.ps1
@@ -1,5 +1,8 @@
 $folderPath = 'E:\OneDrive\Documents2\My Games\FarmingSimulator2022'
 $backupPath = 'E:\OneDrive\Documents2'
+
+# DO NOT EDIT ANYTHING BELOW THIS LINE!!!
+
 $readonly = Get-ItemProperty $folderPath\inputBinding.xml | Select-Object IsReadOnly
 
 if ( $readonly -like '*False*' )
